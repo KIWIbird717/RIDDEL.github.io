cbde030a63e35bfe5227816c169e9850683058d4
KIWIbird717 <77553744+KIWIbird717@users.noreply.github.com>
2022-12-06 16:07:56 +0300

    Update Collection.tsx

--- components/Sections/Collection/Collection.tsx
+++ components/Sections/Collection/Collection.tsx
@@ -102,7 +102,7 @@ export const Collection = ({ ...props }: CollectionProps): JSX.Element => {
                   style={{
                     transform: active === index ? 'scale(0.7)' : 'scale(0.4)',
                   }}
-                  src={`/collectionbike/${image}.png`}
+                  src={`public/collectionbike/${image}.png`}
                   alt={`Bicycle Image`}
                 />
               </Grid>
